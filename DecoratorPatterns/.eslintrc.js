3819e27ce3beaab0df3eee45a6d8d95b7f2a6cac
ag502 <ag502@naver.com>
2022-08-16 23:21:06 +0900

    fix: eslint 수정

--- DecoratorPatterns/.eslintrc.js
+++ DecoratorPatterns/.eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
 	env: {
+		node: true,
 		browser: true,
 		es2021: true,
 	},
-	extends: ['standard-with-typescript', 'plugin:prettier/recommended'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
 	overrides: [],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
+	plugins: ['@typescript-eslint'],
 	rules: {
 		'prettier/prettier': [
 			'error',
