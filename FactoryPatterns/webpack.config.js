01ba7713e2a298e04b3d1cbbd34691a4a54fc751
ag502 <ag502@naver.com>
2022-09-03 13:55:00 +0900

    add: 초기 설정

---
+++ FactoryPatterns/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devtool: "eval-source-map",
+  entry: "./src/index.ts",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    hot: false,
+  },
+  output: {
+    publicPath: "/",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+};
