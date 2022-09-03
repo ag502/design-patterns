7546e5ffd9816a15724651a468163e5515517dab
ag502 <ag502@naver.com>
2022-09-04 02:12:59 +0900

    add: StereoOnWithCDCommand 추가

---
+++ CommandPatterns/src/Command/StereoOnWithCDCommand.ts
@@ -0,0 +1,18 @@
+import Command from "./InterfaceCommand";
+import Stereo from "../Device/InterfaceStereo";
+
+class StereoOnWithCDCommand implements Command {
+  private stereo: Stereo;
+
+  constructor(stereo: Stereo) {
+    this.stereo = stereo;
+  }
+
+  execute() {
+    this.stereo.on();
+    this.stereo.setCd("깊은 밤을 날아서");
+    this.stereo.setVolume(11);
+  }
+}
+
+export default StereoOnWithCDCommand;
