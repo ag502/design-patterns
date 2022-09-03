057d4840048fa135165e454a99f07953b5127fe7
ag502 <ag502@naver.com>
2022-09-03 15:39:18 +0900

    add: 뉴욕 피자 가게 추가

---
+++ FactoryPatterns/src/PizzaStore/NYPizzaStore.ts
@@ -0,0 +1,15 @@
+import PizzaStore from "./AbstractPizzaStore";
+import Pizza from "../Pizza/InterfacePizza";
+
+import NYStyleCheesePizza from "../Pizza/NYStyleCheesePizza";
+
+class NYPizzaStore extends PizzaStore {
+  createPizza(type: string): Pizza | null {
+    if (type === "chees") {
+      return new NYStyleCheesePizza();
+    }
+    return null;
+  }
+}
+
+export default NYPizzaStore;
