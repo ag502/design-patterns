33ac55e73075b3e41fb1dd52f05298c4e297e919
ag502 <ag502@naver.com>
2022-09-03 13:41:57 +0900

    add: 행동 실행

--- DecoratorPatterns/src/index.ts
+++ DecoratorPatterns/src/index.ts
@@ -1,1 +1,14 @@
-console.log('hi');
+import DarkRoast from './Beverages/DarkRoast';
+import Espresso from './Beverages/Espresso';
+import Mocha from './Decorator/Mocha';
+import Whip from './Decorator/Whip';
+
+const espresso = new Espresso();
+console.log(`${espresso.beverageDescription} $ ${espresso.cost()}`);
+
+const darkRoast = new DarkRoast();
+const darkRoastWithMocha = new Mocha(darkRoast);
+const darkRoastWithDoubleMocha = new Mocha(darkRoastWithMocha);
+const darkRoastWithDoubleMochaWithWhip = new Whip(darkRoastWithDoubleMocha);
+
+console.log(`${darkRoastWithDoubleMochaWithWhip.beverageDescription} $ ${darkRoastWithDoubleMochaWithWhip.cost()}`);
