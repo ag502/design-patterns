2d9857b452477f0514062dd134237fda0579e022
ag502 <ag502@naver.com>
2023-01-10 00:57:19 +0900

    add: 디자인 패턴 링크 추가

---
+++ typescript/src/StrategyPattern/index.ts
@@ -0,0 +1,9 @@
+class Strategy {
+    constructor() {
+        console.log("HI IM STRATEGY");
+    }
+}
+
+const StrategyInstance = new Strategy();
+
+export default StrategyInstance;
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,16 @@
-console.log("HELLO");
+const patterns = [{ name: "Strategy Pattern", src: "/strategy-pattern" }];
+
+const links = patterns
+    .map(({ name, src }) => {
+        return /* html */ `
+        <a href=${src}>${name}</a>
+    `;
+    })
+    .join("");
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+document.querySelector("#app")!.innerHTML = links;
+
+if (location.pathname === "/strategy-pattern") {
+    import("./StrategyPattern/index");
+}
